127645d8170d750d8fed54926ba5f0650ee59a0d
Surabhi7602 <sshetty72@gatech.edu>
2023-04-04 18:23:08 -0400

    basic functionality

--- top-album-poster-generator/src/Main.js
+++ top-album-poster-generator/src/Main.js
@@ -8,23 +8,16 @@ const spotifyApi = new SpotifyWebApi();
 function Main() {
   const [token, setToken] = useState(null);
   const [topTracks, setTopTracks] = useState([]);
-  const [albumCovers, setAlbumCovers] = useState([]);
+
+  const [base, setBase] = useState(null);
 
   const onAccessToken = (accessToken) => {
     setToken(accessToken);
     spotifyApi.setAccessToken(accessToken);
   };
 
-  useEffect(() => {
-    if (token) {
-      getTopTracks();
-    }
-  }, [token]);
-
   async function getTopTracks() {
     try {
-      console.log("spotifyApi:", spotifyApi); // Add this line
-      console.log("accessToken:", token); // Add this line
       const response = await spotifyApi.getMyTopTracks({
         time_range: "long_term",
         limit: 16,
@@ -56,53 +49,105 @@ function Main() {
         }
       }
     }
-    setAlbumCovers(coverUrls);
-    console.log(coverUrls);
+
+    generatePos(coverUrls);
   }
 
-  // function renderAlbumCovers() {
-  //   const rows = [];
-  //   for (let i = 0; i < albumCovers.length; i += 4) {
-  //     const row = [];
-  //     for (let j = 0; j < 4 && i + j < albumCovers.length; j++) {
-  //       const url = albumCovers[i + j];
-  //       row.push(<img key={i + j} src={url} alt={`Album cover ${i + j}`} />);
-  //     }
-  //     rows.push(
-  //       <div key={i} className="row">
-  //         {row}
-  //       </div>
-  //     );
-  //   }
-  //   return <div className="grid">{rows}</div>;
-  // }
+  function imgLoad(url) {
+    return new Promise(function (resolve, reject) {
+      var img = new Image();
+      img.crossOrigin = "Anonymous";
+      img.src = url;
+      img.onload = function () {
+        resolve(img);
+      };
+      img.onerror = reject;
+    });
+  }
+
+  function generatePos(covers) {
+    // map an array of promises calling imgLoad() for each url
+    let imageLoadpromises = covers.map(imgLoad);
+
+    Promise.all(imageLoadpromises)
+      .then(function (images) {
+        //images is array of image elements from above
+
+        var canvas = document.createElement("canvas");
+        canvas.width = 2048;
+        canvas.height = 2048;
+
+        var ctx = canvas.getContext("2d");
+        var offsetY = 0;
+
+        for (var i = 0; i < images.length; i += 4) {
+          var rowImages = images.slice(i, i + 4);
+          var offsetX = 0;
+          for (var j = 0; j < rowImages.length; j++) {
+            var img = rowImages[j];
+            ctx.drawImage(img, offsetX, offsetY, 512, 512);
+            offsetX += 512;
+          }
+          offsetY += 512;
+        }
+
+        let b64Data = canvas.toDataURL();
+        setBase(b64Data);
+      })
+      .catch(function (err) {
+        console.log("One or more images did not load");
+      });
+  }
+
+  function linkfunction(b64Data) {
+    var link = document.createElement("a");
+
+    var blob = dataURLtoBlob(b64Data);
+    var objurl = URL.createObjectURL(blob);
+
+    link.download = "Top16Tracks.png";
+
+    link.href = objurl;
+
+    link.click();
+  }
+
+  function dataURLtoBlob(dataurl) {
+    var arr = dataurl.split(","),
+      mime = arr[0].match(/:(.*?);/)[1],
+      bstr = atob(arr[1]),
+      n = bstr.length,
+      u8arr = new Uint8Array(n);
+    while (n--) {
+      u8arr[n] = bstr.charCodeAt(n);
+    }
+    return new Blob([u8arr], { type: mime });
+  }
 
   return (
     <div>
-      {token ? (
-        <div>
-          <p>{token}</p>
-          {topTracks.length > 0 && (
-            <ul>
-              {topTracks.map((track) => (
-                <li key={track.id}>{track.name}</li>
-              ))}
-            </ul>
-          )}
-          {/* {albumCovers.length > 0 && renderAlbumCovers()} */}
-        </div>
+      <div>
+        {token ? (
+          <button onClick={() => getTopTracks()}>Get Top Tracks</button>
+        ) : (
+          <SpotifyAuth
+            clientID="ed8e6a84c4d94b31a47fa7f4d8e8c274"
+            redirectUri="http://localhost:3000/callback"
+            scopes={[
+              "streaming",
+              "user-read-private",
+              "user-read-email",
+              "user-top-read",
+            ]}
+            onAccessToken={onAccessToken}
+          />
+        )}
+      </div>
+
+      {base ? (
+        <button onClick={() => linkfunction(base)}>Download Image</button>
       ) : (
-        <SpotifyAuth
-          clientID="ed8e6a84c4d94b31a47fa7f4d8e8c274"
-          redirectUri="http://localhost:3000/callback"
-          scopes={[
-            "streaming",
-            "user-read-private",
-            "user-read-email",
-            "user-top-read",
-          ]}
-          onAccessToken={onAccessToken}
-        />
+        <div></div>
       )}
     </div>
   );
