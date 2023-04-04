7a840994a03c22c2be8c63f9ac8722a1c3195f7f
Surabhi7602 <sshetty72@gatech.edu>
2023-04-04 15:12:10 -0400

    new

--- top-album-poster-generator/src/Main.js
+++ top-album-poster-generator/src/Main.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { SpotifyAuth, Scopes } from "react-spotify-auth";
 import SpotifyWebApi from "spotify-web-api-js";
-import axios from "axios";
+import "./Main.css";
 
 const spotifyApi = new SpotifyWebApi();
 
@@ -31,11 +31,51 @@ function Main() {
       });
       setTopTracks(response.items);
       console.log(response.items);
+      if (response.items.length > 0) {
+        generateAlbumUrls(response.items);
+      }
     } catch (err) {
       console.error(spotifyApi.getAccessToken);
       console.error("Error retrieving top tracks:", err.response);
     }
   }
+
+  function generateAlbumUrls(items) {
+    const coverUrls = [];
+    for (const track of items) {
+      const album = track.album;
+      if (album) {
+        const images = album.images;
+        if (images && images.length > 0) {
+          for (const image of images) {
+            if (image.height === 640) {
+              coverUrls.push(image.url);
+              break;
+            }
+          }
+        }
+      }
+    }
+    setAlbumCovers(coverUrls);
+    console.log(coverUrls);
+  }
+
+  // function renderAlbumCovers() {
+  //   const rows = [];
+  //   for (let i = 0; i < albumCovers.length; i += 4) {
+  //     const row = [];
+  //     for (let j = 0; j < 4 && i + j < albumCovers.length; j++) {
+  //       const url = albumCovers[i + j];
+  //       row.push(<img key={i + j} src={url} alt={`Album cover ${i + j}`} />);
+  //     }
+  //     rows.push(
+  //       <div key={i} className="row">
+  //         {row}
+  //       </div>
+  //     );
+  //   }
+  //   return <div className="grid">{rows}</div>;
+  // }
 
   return (
     <div>
@@ -49,6 +89,7 @@ function Main() {
               ))}
             </ul>
           )}
+          {/* {albumCovers.length > 0 && renderAlbumCovers()} */}
         </div>
       ) : (
         <SpotifyAuth
